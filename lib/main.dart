f68c56c60ba02bd8fcdde5c39036a906c3ae355f
yangoff <yangovip@gmail.com>
2019-01-28 14:26:16 -0200

    quase pronto

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:giphylocalizator/ui/home_page.dart';
+import 'package:giphylocalizator/ui/gif_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
